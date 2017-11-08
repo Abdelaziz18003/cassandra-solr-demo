8008199675ac1c1b82640a26832175ce9884abd3
abdelaziz <m.abdelaziz1993@gmail.com>
2017-11-08 14:15:15 +0100

    add insert book

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cassandra = require('cassandra-driver');
+var uuid = cassandra.types.Uuid.random;
 
 // create the server
 var app = express();
 app.listen(3000);
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // connect to the cassandra database
 var client = new cassandra.Client({
@@ -23,10 +25,30 @@ client.connect((err) => {
   console.log('server started at http://localhost:3000 ...');
 });
 
-// handle requests
+// home route
 app.get('/', (req, res) => {
   res.json({
     success: true,
     message: 'welcome to cassandra and solr integration demo'
+  });
+});
+
+// insert book
+app.post('/insert', (req, res) => {
+  var query = `INSERT INTO books (id, title, description) VALUES (${uuid()}, '${req.body.title}', '${req.body.description}')`;
+  console.log(req.body)
+  client.execute(query, (err) => {
+    if (err) {
+      res.json({
+        success: false,
+        message: result.message
+      })
+    }
+    else {
+      res.json({
+        success: true,
+        message: 'book inserted successfully'
+      })
+    }
   });
 });
